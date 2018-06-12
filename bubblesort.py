54ad96f0f65bb12a8f4bb315bd37efc95b61957a
Nicholas Willard <nswillard@hotmail.com>
2018-06-11 23:17:14 -0500

    Fixed a broken script I wrote 4 years ago.

--- bubblesort.py
+++ bubblesort.py
@@ -5,21 +5,9 @@ from random import shuffle
 
 def get_list():
     """Takes data from the user and creates a list."""
-    err = True
     print("Enter your numbers.\nLeave a space between each one.")
-    while err == True:
-        err = False
-        temp = input("Data:")
-        my_list = temp.split()
-        for item in my_list:
-            try:
-                item = float(item)
-            except:
-                print("Try again. Only numbers and spaces please.")
-                err = True
-                break
+    my_list = [int(number) for number in raw_input("Enter integers: ").split()]
     return my_list
-
 #main
 print("Welcome to Bubble Sort.")
 unsorted = []
@@ -32,22 +20,22 @@ while ans != 'q':
         1: Use numbers 1-20 shuffled.
         2: Input your own values.
         q: Quit.""")
-    ans = input("\nWhat do you want to do?")
-    if ans == 'q':
+    ans = raw_input("\nWhat do you want to do? ")
+    if str(ans) == 'q':
         continue
-    elif ans == '1':
+    elif int(ans) == 1:
         #initialise the list
         unsorted = list(range(20))
         #shuffle the numbers
         shuffle(unsorted)
         print("Randomised list created")
-    elif ans == '2':
+    elif int(ans) == 2:
         unsorted = get_list()
+        #shuffle the numbers
+        shuffle(unsorted)
         print("You gave me:")
     else:
         print("Unrecognised choice.")
         continue
     print(unsorted)
 
-
-                                                             
