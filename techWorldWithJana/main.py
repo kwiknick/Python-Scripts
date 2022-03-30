5df34cc7006633fc127b64532086dd1e0bfab381
Nicholas Willard <nswillard@hotmail.com>
2022-03-29 21:24:58 -0500

    Add files and demonstrate object oriented Python using Classes

--- techWorldWithJana/main.py
+++ techWorldWithJana/main.py
@@ -1,55 +1,11 @@
-import openpyxl
-from copy import copy
-import os
+from user import User
+from post import Post
 
-inv_file = openpyxl.load_workbook("inventory.xlsx")
-product_list = inv_file["Sheet1"]
+firstUser = User("nick@willard.com", "Nick Willard", "secretPassword", "BizOps Engineer")
+firstUser.get_user_info()
 
-products_per_supplier = {}
-total_value_per_supplier = {}
-products_under_10_inv = {}
+secondUser = User("john@willard.com", "John Willard", "secretPassword", "BizOps Engineer")
+secondUser.get_user_info()
 
-# Get the style from the other header column and copy to the newly added column
-col_4 = product_list.cell(1, 4)
-product_list.cell(1, 5).font = copy(col_4.font)
-product_list.cell(1, 5).fill = copy(col_4.fill)
-product_list.cell(1, 5).value = 'Inventory Price'
-
-
-for product_row in range(2, product_list.max_row + 1):
-    supplier_name = product_list.cell(product_row, 4).value
-    inventory = product_list.cell(product_row, 2).value
-    price = product_list.cell(product_row, 3).value
-    product_number = product_list.cell(product_row, 1).value
-    inventory_price = product_list.cell(product_row, 5)
-
-    # calculation number of products per supplier
-    if supplier_name in products_per_supplier:
-        products_per_supplier[supplier_name] += 1
-    else:
-        products_per_supplier[supplier_name] = 1
-
-    # calculation total value of inventory per supplier
-    if supplier_name in total_value_per_supplier:
-        total_value_per_supplier[supplier_name] += inventory * price
-    else:
-        total_value_per_supplier[supplier_name] = inventory * price
-
-    # logic products with inventory less than 10
-    if inventory < 10:
-        products_under_10_inv[int(product_number)] = int(inventory)
-
-    # Add a column to excel file for total inventory price
-    inventory_price.value = inventory * price
-
-print(products_per_supplier)
-print(total_value_per_supplier)
-print(products_under_10_inv)
-
-# Haven't been able to get the column width thing to work yet...
-# product_list.column_dimensions["E"].auto_size = True
-
-if not os.path.exists(".\\generated"):
-    os.makedirs(".\\generated")
-
-inv_file.save(".\\generated\\inventory_with_total_value.xlsx")
+new_post = Post("on a secret mission today", secondUser.name)
+new_post.get_post_info()
